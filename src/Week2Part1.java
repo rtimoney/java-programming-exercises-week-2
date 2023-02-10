d99852ea340e41357b53c6f3d06d411a38422bac
rtimoney <103318517+rtimoney@users.noreply.github.com>
2023-02-10 19:42:32 +0000

    Update Week2Part1.java

--- src/Week2Part1.java
+++ src/Week2Part1.java
@@ -1,17 +1,8 @@
 /**
- * 
- */
-
-/**
  * @author Ronan J Timoney
- *
  * Main method for Part1 for Practical 2
- *
  */
 public class Week2Part1 {
-	
-
-
 	/**
 	 * @param args
 	 */
@@ -24,8 +15,7 @@ public class Week2Part1 {
 
 		System.out.println("The input String : " + input);
 		System.out.println("The number of Characters : "+input.length());
-		System.out.printf("The first character of the string is : %s and the secong is : %s%n", input.charAt(0),
-				input.charAt(1));
+		System.out.printf("The first character of the string is : %s and the secong is : %s%n", input.charAt(0), input.charAt(1));
 		
 		System.out.println("The first character in the string is " + input.charAt(0)
 		+ " and the second character in the string is : " + input.charAt(1));
@@ -35,28 +25,6 @@ public class Week2Part1 {
 		
 		// 
 		
-		System.out.println("The string with fada added is : " + input.replace('o','ó'));
-		
-		
-		
-		
-				
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-
-}
+		System.out.println("The string with fada added is : " + input.replace('o','Ã³'));		
+	} // end of main method
+} // end of class
